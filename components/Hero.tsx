9ff424ecadbf91aad48cb3610f8197f5b9d6909c
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-12-11 18:42:31 +0300

    Refactor Hero and InputArea components for improved styling and functionality

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,23 +19,14 @@ const Hero: FC<THeroProps> = ({
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <a
-        className="mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-white px-3 py-4 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
-        href="https://www.together.ai/"
-        target="_blank"
-      >
-        <Image
-          unoptimized
-          src="/img/together-ai.svg"
-          alt="hero"
-          width={18}
-          height={18}
-        />
-        <span className="text-center text-base font-light leading-[normal] text-[#1B1B16]">
-          Supported by Together AI
-        </span>
-      </a>
-      <h2 className="bg-custom-gradient bg-clip-text pb-7 pt-2 text-center text-3xl font-semibold leading-[normal] lg:text-[64px]">
+    <Image
+      src="/hero.png"
+      alt="Descriptive alt text"
+      className="mx-auto rounded-lg w-full max-w-[600px] mt-4"
+      width={600}
+      height={400}
+    />
+      <h2 className="mt-4 bg-custom-gradient bg-clip-text pb-7 pt-2 text-center text-3xl font-semibold leading-[normal] lg:text-[64px]">
         Surf with purpose
       </h2>
 
@@ -72,7 +63,7 @@ const Hero: FC<THeroProps> = ({
       </div>
 
       {/* Github link section */}
-      <p className="text-center text-sm font-light leading-[normal] text-[#1B1B16]">
+      <p className="mt-8 text-center text-sm font-light leading-[normal] text-[#1B1B16]">
         Follow us!{" "}
         <span className="text-sm font-medium underline">
           <a
@@ -84,6 +75,23 @@ const Hero: FC<THeroProps> = ({
           </a>
         </span>
       </p>
+
+      <a
+        className="mt-16 mb-4 inline-flex h-7 shrink-0 items-center gap-[9px] rounded-[50px] border-[0.5px] border-solid border-[#E6E6E6] bg-white px-3 py-4 shadow-[0px_1px_1px_0px_rgba(0,0,0,0.25)]"
+        href="https://www.together.ai/"
+        target="_blank"
+      >
+        <Image
+          unoptimized
+          src="/img/together-ai.svg"
+          alt="hero"
+          width={18}
+          height={18}
+        />
+        <span className="text-center text-base font-light leading-[normal] text-[#1B1B16]">
+          Credits: Together AI
+        </span>
+      </a>
     </div>
   );
 };
